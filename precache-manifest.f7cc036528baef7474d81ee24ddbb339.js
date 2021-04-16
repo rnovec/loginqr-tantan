54177c060adb54c52104c4e83611b94e8a8b5c2a
Raul Novelo <rnovelo.cruz98@gmail.com>
2021-04-16 16:07:56 -0500

    Updates

---
+++ precache-manifest.f7cc036528baef7474d81ee24ddbb339.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a84479228ca46d3f0f5",
+    "url": "/loginqr-tantan/css/chunk-vendors.564f03f9.css"
+  },
+  {
+    "revision": "d8e9f5aebcf2789239cbc08618f874f0",
+    "url": "/loginqr-tantan/favicon.png"
+  },
+  {
+    "revision": "3715feea957f70ff4ed8d641c5654f1e",
+    "url": "/loginqr-tantan/index.html"
+  },
+  {
+    "revision": "338dd04a993fdc5c5102",
+    "url": "/loginqr-tantan/js/app.f889bfa7.js"
+  },
+  {
+    "revision": "8a84479228ca46d3f0f5",
+    "url": "/loginqr-tantan/js/chunk-vendors.b5398644.js"
+  },
+  {
+    "revision": "d718c2d902e44ec1b2d35b4a2d93ab19",
+    "url": "/loginqr-tantan/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/loginqr-tantan/robots.txt"
+  },
+  {
+    "revision": "65dc94066f60f9c6233a1522e553b772",
+    "url": "/loginqr-tantan/service-worker.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,40 @@
+importScripts("/loginqr-tantan/precache-manifest.f7cc036528baef7474d81ee24ddbb339.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.setConfig({
+  debug: false
+})
+
+workbox.precaching.precacheAndRoute([])
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+      })
+    ]
+  })
+)
+
+// workbox.routing.registerRoute(
+//   new RegExp('https://digital-window.herokuapp.com'),
+//   workbox.strategies.networkFirst({
+//     cacheName: 'api'
+//   })
+// )
+
+workbox.routing.registerRoute(
+  new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+  workbox.strategies.cacheFirst({
+    cacheName: 'googleapis',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 30
+      })
+    ]
+  })
+)
+
